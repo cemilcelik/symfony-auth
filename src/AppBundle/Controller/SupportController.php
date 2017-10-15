0f32f61ab97fd8e035c732c0b473dab4a2d74522
cemilcelik <cemilcelik84@gmail.com>
2017-10-15 11:47:58 +0300

    Contact form build, bootstrap load, mail send

---
+++ src/AppBundle/Controller/SupportController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+
+class SupportController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('from', EmailType::class)
+            ->add('message', TextareaType::class)
+            ->add('send', ButtonType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+
+            dump($data);
+            
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Symfony Contact Message')
+                ->setFrom($data['from'])
+                ->setTo($this->container->getParameter('adminEmail'))
+                ->setBody(
+                    $data['message'],
+                    'text/plain'
+                );
+            
+            $this->get('mailer')->send($message);
+
+        }
+        
+        // replace this example code with whatever you need
+        return $this->render('support/index.html.twig', ['ourForm' => $form->createView()]);
+    }
+}
