ad9d2357ae96d489b0f7924f16bc90d840dbcdf9
cemilcelik <cemilcelik84@gmail.com>
2017-10-15 18:37:07 +0300

    Registeration complete. Set member entity encode algorithtm. Add flash message.

--- src/AppBundle/Controller/RegisterationController.php
+++ src/AppBundle/Controller/RegisterationController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Form\MemberType;
 use AppBundle\Entity\Member;
 
@@ -12,11 +13,35 @@ class RegisterationController extends Controller {
     /**
      * @Route("/register", name="registeration")
      */
-    public function registerAction()
+    public function registerAction(Request $request)
     {
         $member = new Member();
 
         $form = $this->createForm(MemberType::class, $member);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $password = $this
+                ->get('security.password_encoder')
+                ->encodePassword(
+                    $member,
+                    $member->getPlainPassword()
+                )
+            ;
+
+            $member->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($member);
+            $em->flush();
+
+            $this->addFlash('success', 'You are now successfully registered!');
+
+            return $this->redirectToRoute('homepage');
+
+        }
 
         return $this->render('registeration/register.html.twig', [
             'registeration_form' => $form->createView()
